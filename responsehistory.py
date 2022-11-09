f1def949dc48bd67bc2cf6bb5289c1193b387a34
aleks21977 <afonin_av@list.ru>
2022-11-09 18:44:24 +0300

    responsehistory2

--- responsehistory.py
+++ responsehistory.py
@@ -3,6 +3,5 @@ import requests
 
 response = requests.get("https://playground.learnqa.ru/api/long_redirect")
 count_responses = len(response.history)
-print(f"Количество запросов = {count_responses}")
-if count_responses > 1:
-    print(f"Последний URL - {response.history[(int(count_responses) - 1)].url}")
+print(f"Количество редиректов = {count_responses}")
+print(f"Последний URL - {response.url}")
