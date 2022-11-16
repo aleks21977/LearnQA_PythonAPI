822def922e792bd720df11722e9dac8b308c0179
aleks21977 <afonin_av@list.ru>
2022-11-16 21:27:36 +0300

    Промежуточный коммит после просмотра лекций третьего занятия

--- responsehistory.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-
-response = requests.get("https://playground.learnqa.ru/api/long_redirect")
-count_responses = len(response.history)
-print(f"Количество редиректов = {count_responses}")
-print(f"Последний URL - {response.url}")
